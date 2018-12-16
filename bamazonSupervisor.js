1eef36bc8bbb1dd86fe390782ef25b5a56a90a94
Kaleb Kougl <kalebkougl@gmail.com>
2018-12-16 12:32:41 -0600

    adding department schema

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -37,3 +37,7 @@ function afterConnection() {
 
   connection.end();
 }
+
+function viewSales() {
+  
+}
---
+++ departmentSchema.sql
@@ -0,0 +1,9 @@
+-- CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE departments (
+	department_id INTEGER(11) NOT NULL AUTO_INCREMENT , 
+    department_name VARCHAR(25) NOT NULL,
+    over_head_costs DOUBLE(10,2) NOT NULL,
+    PRIMARY KEY (department_id)
+);
