529dbe787929d984ae8abddace8220e04a6156d8
Kaleb Kougl <kalebkougl@gmail.com>
2018-12-16 13:04:13 -0600

    adding default value to over_head_costs

--- departmentSchema.sql
+++ departmentSchema.sql
@@ -2,8 +2,8 @@
 USE bamazon;
 
 CREATE TABLE departments (
-	department_id INTEGER(11) NOT NULL AUTO_INCREMENT , 
+	department_id INTEGER(11) NOT NULL AUTO_INCREMENT, 
     department_name VARCHAR(25) NOT NULL,
-    over_head_costs DOUBLE(10,2) NOT NULL,
+    over_head_costs DOUBLE(10,2) NOT NULL DEFAULT 1000,
     PRIMARY KEY (department_id)
 );
